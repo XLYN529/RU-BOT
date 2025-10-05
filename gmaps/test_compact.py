b661e762b6e08d5ff0fc314d50ecda78e3d4b30b
aks291 <aks291@scarletmail.rutgers.edu>
2025-10-04 23:52:57 -0400

    Updated API

--- gmaps/test_compact.py
+++ gmaps/test_compact.py
@@ -1,3 +1,3 @@
 # test_compact.py
 from rutgers_busyness import compact_answer
-print(compact_answer("how crowded is College Ave Student Center around 4am?"))
+print(compact_answer("how crowded is College Ave around 4am?"))
